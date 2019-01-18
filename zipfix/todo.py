dc47c5e53e9eed0097998ff4977567a3046486d0
Manish Goregaokar <manishsmail@gmail.com>
2019-01-18 18:38:29 -0500

    Step.parse()

--- zipfix/todo.py
+++ zipfix/todo.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from typing import List
-from .odb import Commit
+from .odb import Commit, Repository
+import re
 
 class StepKind(Enum):
     PICK = 1
@@ -19,6 +20,20 @@ class StepKind(Enum):
             return 'index'
         raise TypeError()
 
+    @staticmethod
+    def parse(s: str) -> StepKind:
+        if len(s) < 1:
+            raise ValueError()
+        if 'pick'.startswith(s):
+            return StepKind.PICK
+        if 'fixup'.startswith(s):
+            return StepKind.FIXUP
+        if 'reword'.startswith(s):
+            return StepKind.REWORD
+        if 'index'.startswith(s):
+            return StepKind.INDEX
+        raise ValueError()
+
 
 class Step:
     # pick SHA1... Commit Message Text FOr Helpy Stuff
@@ -28,7 +43,18 @@ class Step:
     def __str__(self):
         return f"{self.kind} {self.commit.tree_oid.short()} {self.commit.summary()}"
 
+    def __init__(self, kind: StepKind, commit: Commit):
+        self.kind = kind
+        self.commit = commit
 
+    @staticmethod
+    def parse(repo: Repository, s: str) -> Step:
+        parsed = re.match('(?P<command>\S+)\s(?P<hash>\S+)', s)
+        if not parsed:
+            raise ValueError()
+        kind = StepKind.parse(parsed.group('command'))
+        commit = repo.getcommit(parsed.group('hash'))
+        return Step(kind, commit)
 
 def thingy(list: List[Commit], include_index: bool) -> List[Step]:
     # Prompt the user
