8d898ccb87ed676471e4a97374dff9a8dbdc48b4
Nika Layzell <nika@thelayzells.com>
2019-01-26 16:22:49 -0500

    Re-add manpages_url

--- docs/conf.py
+++ docs/conf.py
@@ -97,7 +97,7 @@ html_theme = "sphinx_rtd_theme"
 #
 # html_sidebars = {}
 
-# manpages_url = "https://manpages.debian.org/{path}"
+manpages_url = "https://manpages.debian.org/{path}"
 
 # -- Options for manual page output ------------------------------------------
 
