b7dfdebdfc011a0a393ba9445e37d7f8e9fb6651
Nika Layzell <nika@thelayzells.com>
2019-09-28 23:16:09 -0400

    parametrize reword tests to simplify them

--- tests/test_reword.py
+++ tests/test_reword.py
@@ -4,37 +4,42 @@ import textwrap
 from conftest import *
 
 
-@pytest.fixture
-def basic_repo(repo, bash):
+@pytest.mark.parametrize("target", ["HEAD", "HEAD~", "HEAD~~"])
+@pytest.mark.parametrize("use_editor", [True, False])
+def test_reword(repo, bash, main, target, use_editor):
     bash(
         """
-        cat <<EOF >file1
-        Hello, World!
-        How are things?
-        EOF
+        echo "hello, world" > file1
         git add file1
-        git commit -m "commit1"
-
-        cat <<EOF >file1
-        Hello, World!
-        Oops, gotta add a new line!
-        How are things?
-        EOF
+        git commit -m "commit 1"
+        echo "new line!" >> file1
         git add file1
-        git commit -m "commit2"
+        git commit -m "commit 2"
+        echo "yet another line!" >> file1
+        git add file1
+        git commit -m "commit 3"
         """
     )
-    return repo
 
+    message = textwrap.dedent(
+        """\
+        reword test
 
-def reword_helper(repo, main, flags, target, message):
-    message = textwrap.dedent(message).encode()
+        another line
+        """
+    ).encode()
 
     old = repo.get_commit(target)
     assert old.message != message
     assert old.persisted
 
-    main(flags + [target])
+    if use_editor:
+        with Editor() as ed, in_parallel(main, ["--no-index", "-e", target]):
+            with ed.next_file() as f:
+                assert f.startswith(old.message)
+                f.replace_dedent(message)
+    else:
+        main(["--no-index", "-m", "reword test", "-m", "another line", target])
 
     new = repo.get_commit(target)
     assert old != new, "commit was modified"
@@ -44,79 +49,3 @@ def reword_helper(repo, main, flags, target, message):
     assert new.message == message, "message set correctly"
     assert new.persisted, "commit persisted to disk"
     assert new.author == old.author, "author is unchanged"
-    assert new.committer == repo.default_committer, "committer is updated"
-
-
-def test_reword_head(basic_repo, main):
-    reword_helper(
-        basic_repo,
-        main,
-        ["--no-index", "-m", "reword_head test", "-m", "another line"],
-        "HEAD",
-        "reword_head test\n\nanother line\n",
-    )
-
-
-def test_reword_nonhead(basic_repo, main):
-    reword_helper(
-        basic_repo,
-        main,
-        ["--no-index", "-m", "reword_nonhead test", "-m", "another line"],
-        "HEAD~",
-        "reword_nonhead test\n\nanother line\n",
-    )
-
-
-def test_reword_head_editor(basic_repo, main):
-    old = basic_repo.get_commit("HEAD")
-    new_message = """\
-        reword_head_editor test
-
-        another line
-        """
-
-    with Editor() as ed, in_parallel(
-        reword_helper, basic_repo, main, ["--no-index", "-e"], "HEAD", new_message
-    ):
-        with ed.next_file() as f:
-            assert f.startswith(old.message)
-            f.replace_dedent(new_message)
-
-
-def test_reword_nonhead_editor(basic_repo, main):
-    old = basic_repo.get_commit("HEAD~")
-    new_message = """\
-        reword_nonhead_editor test
-
-        another line
-        """
-
-    with Editor() as ed, in_parallel(
-        reword_helper, basic_repo, main, ["--no-index", "-e"], "HEAD~", new_message
-    ):
-        with ed.next_file() as f:
-            assert f.startswith(old.message)
-            f.replace_dedent(new_message)
-
-
-def test_reword_root(repo, main, bash):
-    bash(
-        """
-        echo "hello, world" > file1
-        git add file1
-        git commit -m "initial commit"
-        echo "new line!" >> file1
-        git add file1
-        git commit -m "another commit"
-        """
-    )
-
-    old = repo.get_commit("HEAD~")
-    assert old.parents() == []
-    assert old.message == b"initial commit\n"
-
-    main(["-m", "my new message", "HEAD~"])
-
-    new = repo.get_commit("HEAD~")
-    assert new.parents() == []
-    assert new.message == b"my new message\n"
