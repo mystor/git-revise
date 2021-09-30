44a2182a0afb520a7a4c5b4127f06cef0f669c97
Robert Estelle <robertestelle@gmail.com>
2021-09-30 11:01:32 -0700

    test.edit: Factor out dummy editor main function

--- tests/dummy_editor.py
+++ tests/dummy_editor.py
@@ -3,8 +3,7 @@ from pathlib import Path
 from urllib.request import urlopen
 
 
-if __name__ == "__main__":
-    path = Path(sys.argv[1]).resolve()
+def run_editor(path: Path) -> None:
     # pylint: disable=invalid-name
     with urlopen("http://127.0.0.1:8190/", data=path.read_bytes(), timeout=5) as r:
         length = int(r.headers.get("content-length"))
@@ -12,3 +11,7 @@ if __name__ == "__main__":
         if r.status != 200:
             raise Exception(data.decode())
     path.write_bytes(data)
+
+
+if __name__ == "__main__":
+    run_editor(path=Path(sys.argv[1]).resolve())
